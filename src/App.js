2da11dfa0d8ddd90ec1dd66bc772a1fc3850bcd0
Yulduz Muradova <69774668+YulduzM1@users.noreply.github.com>
2023-12-06 22:59:43 -0500

    Update App.js Hello text.

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <HelloWorld name={'Abu Hossain'}/>
+        <HelloWorld name={'Yulduz'}/>
         <Counter/>
         <FilteredList items={produce}/>
       </div>
